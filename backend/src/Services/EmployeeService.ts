22c6c5406389824131cb41e6121f599bf11911bd
Joussemar Borges <joussemar.borges@gmail.com>
2023-04-06 00:56:37 -0300

    feat: Implementando a função create da EMployeeService

--- backend/src/Services/EmployeeService.ts
+++ backend/src/Services/EmployeeService.ts
@@ -1,8 +1,15 @@
+import { ModelStatic } from "sequelize"; 'sequelize'
 import Employee from "../database/models/employee";
 import IEmployee from "../Interfaces/IEmployee";
 
 class EmployeeService {
+model: ModelStatic<Employee> = Employee;
 
+  async registerEmployee(newEmployeeData: IEmployee): Promise<IEmployee> {
+    const newRegister = await this.model.create({...newEmployeeData})
+
+    return newRegister.dataValues;
+  }
 }
 
 export default EmployeeService;
