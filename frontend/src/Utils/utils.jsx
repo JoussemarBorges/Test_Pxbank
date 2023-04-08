7dfd904b9467ccf5ebb3f68e93927b60214a7f39
Joussemar Borges <joussemar.borges@gmail.com>
2023-04-08 17:57:30 -0300

    refacrtor: alterando a porta do front

--- frontend/src/Utils/utils.jsx
+++ frontend/src/Utils/utils.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosApi = () => {
-  const api = axios.create({baseURL: 'http://localhost:3001'})
+  const api = axios.create({baseURL: process.env.REACT_SERVER_HOST})
   
   return api
   
