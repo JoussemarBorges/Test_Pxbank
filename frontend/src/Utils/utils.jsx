ed1f413126bd0d1edecf876a923ce044d280262c
Joussemar Borges <joussemar.borges@gmail.com>
2023-04-09 11:31:33 -0300

    refacrtor: alterando scripts no npm

--- frontend/src/Utils/utils.jsx
+++ frontend/src/Utils/utils.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
-const HOST = process.env.REACT_APP_API_HOST
-const PROTOCOL = process.env.REACT_APP_API_PROTOCOL
+const HOST = process.env.REACT_APP_API_HOST || 'localhost:3001'
+const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || 'http'
 
 const axiosApi = () => {
   const api = axios.create({baseURL: `${PROTOCOL}://${HOST}`})
